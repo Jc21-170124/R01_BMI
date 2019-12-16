53089ab5f7f7d44685e46a955f574682ba5c1250
桂樹 佐藤 <170124@jc-21.jp>
2019-12-16 09:51:59 +0900

    ソースコードを変更

--- R01_BMI/MainPage.xaml.cs
+++ R01_BMI/MainPage.xaml.cs
@@ -17,5 +17,14 @@ namespace R01_BMI
         {
             InitializeComponent();
         }
+        public void OnButtonClicked(object sender, EventArgs e){
+            
+            int x=Text.tall;
+            int y=Text.heavy;
+            double a=y/(tall/100 * tall/100);
+            answer.Text=a;
+
+        
+        }
     }
 }
