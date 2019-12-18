1697102ec853e9c4e54264043d92a6c7a767811a
桂樹 佐藤 <170124@jc-21.jp>
2019-12-18 12:07:16 +0900

    fff

--- R01_BMI/MainPage.xaml.cs
+++ R01_BMI/MainPage.xaml.cs
@@ -25,7 +25,7 @@ namespace R01_BMI
             double q=double.Parse(y);
 
             double a=q/(ww/100 * ww/100);
-            int aa= Math.Round(a,MidpointRounding.AwayFromZero);
+            double aa= Math.Round(a,MidpointRounding.AwayFromZero);
             answer.Text="BMI:"+aa;
 
         
